d73941fdcebf99b25a1b72651a42ca55f2508f74
SarahA <ashri.sarah@gmail.com>
2021-07-20 12:52:56 +1000

    add React Router
    Details:
    allow routing to different pages/paths.

--- front_end_projects/react_v6_frontendmasters_course/adopt-me/src/App.js
+++ front_end_projects/react_v6_frontendmasters_course/adopt-me/src/App.js
@@ -1,12 +1,25 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Switch>
+          {/* Switch make sure that the router returns only the 1st path that matches.
+              otherwise, it will return (and render) all the paths that match even if there are more than one */}
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
