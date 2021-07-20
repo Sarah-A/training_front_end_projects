08a6d4a06e6d3f1487307bce3ed197c77e5ca96e
SarahA <ashri.sarah@gmail.com>
2021-07-20 13:49:44 +1000

    Support class-properties in class components
    Details:
    In order to remove the constructor from Reach Component Details.js, we need to tell Babel to transpile the new syntax (state = {loading: true};) into the tranditional constructor that calls super() and initialise the instance variable this.state.

--- front_end_projects/react_v6_frontendmasters_course/adopt-me/src/Details.js
+++ front_end_projects/react_v6_frontendmasters_course/adopt-me/src/Details.js
@@ -4,6 +4,8 @@ import { withRouter } from "react-router-dom";
 class Details extends Component {
   state = { loading: true };
 
+  // componentDidMount is called only once by React, after the component renders for the 1st time.
+  // This is usually where you'll do data fetching and processing before displaying it.
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
@@ -17,8 +19,7 @@ class Details extends Component {
       return <h2>loading … </h2>;
     }
 
-    const { animal, breed, city, state, description, name, images } =
-      this.state;
+    const { animal, breed, city, state, description, name } = this.state;
 
     return (
       <div className="details">
@@ -33,4 +34,6 @@ class Details extends Component {
   }
 }
 
+// withRouter is required for this componenet to receive the path-related variable
+// (in this instance, the id)
 export default withRouter(Details);
