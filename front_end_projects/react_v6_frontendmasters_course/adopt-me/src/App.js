627344ba026c4d2871f2453c640bac630f362dd9
SarahA <ashri.sarah@gmail.com>
2021-07-20 13:02:40 +1000

    Switch to <Link> instead of <a> tags
    Details:
    Using <Link> make sure that React treats the links as different pages under the same app. Otherwise, it will reload the entire page.

--- front_end_projects/react_v6_frontendmasters_course/adopt-me/src/App.js
+++ front_end_projects/react_v6_frontendmasters_course/adopt-me/src/App.js
@@ -1,14 +1,16 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
       <Router>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
         <Switch>
           {/* Switch make sure that the router returns only the 1st path that matches.
               otherwise, it will return (and render) all the paths that match even if there are more than one */}
